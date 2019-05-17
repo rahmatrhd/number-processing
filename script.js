a705141684a589dd5b7f3d5a16badf56139bacd1
Rahmat Hidayat <kickyou44@gmail.com>
2019-05-18 01:53:42 +0700

    test

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 function min(arr) {
-
+  var x = arr.sort()
+  return x[0]
+  // return arr.sort()[0]
 }
 
 function max(arr) {
@@ -22,5 +24,5 @@ function numberProcessing(arr) {
   return 'Min: ' + min(arr) + ', Max: ' + max(arr) + ', Mean: ' + mean(arr) + ', Odds: ' + odds(arr) + ', Evens: ' + evens(arr)
 }
 
-console.log(numberProcessing([1, 3, 5, 1, 2, 8, 10, 0, 3]))
+console.log(numberProcessing([1, 3, 5, 1, 2, 8, 10, 1, 3]))
 // "Min: 0, Max: 10, Mean: 4, Odds: 1-3-5-1-3, Evens: 2-8-10-0"

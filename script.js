9418da427d75ee388537e8cb374fac50d1d1b7cd
Rahmat Hidayat <kickyou44@gmail.com>
2019-05-17 21:19:42 +0700

    boilerplate

---
+++ script.js
@@ -0,0 +1,26 @@
+function min(arr) {
+
+}
+
+function max(arr) {
+
+}
+
+function mean(arr) {
+
+}
+
+function odds(arr) {
+
+}
+
+function evens(arr) {
+
+}
+
+function numberProcessing(arr) {
+  return 'Min: ' + min(arr) + ', Max: ' + max(arr) + ', Mean: ' + mean(arr) + ', Odds: ' + odds(arr) + ', Evens: ' + evens(arr)
+}
+
+console.log(numberProcessing([1, 3, 5, 1, 2, 8, 10, 0, 3]))
+// "Min: 0, Max: 10, Mean: 4, Odds: 1-3-5-1-3, Evens: 2-8-10-0"
